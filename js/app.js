3270a3499d1421108f17dc603bffa718256288bc
cheppimdq <cheppimdp@gmail.com>
2024-01-20 17:33:23 -0300

    todo re mal volver atras

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // HEADER SLIDE
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el elemento del nav
+    var nav = document.querySelector('.nav-negro');
+  
+    // Obtener la altura actual del nav
+    var navHeight = nav.offsetHeight;
+  
+    // Aplicar la altura al header
+    var header = document.getElementById('alturaHeader');
+    header.style.height = `calc(100vh - ${navHeight}px)`;
+  });
